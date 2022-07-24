a0cf80662df28f4062e20741630bab11178e0fad
Bruno Roemers <bruno@roemers.io>
2022-07-25 01:18:26 +0200

    Add get involved embed

---
+++ functions/events/discord/message/create/prefix/rubgetinvolved.js
@@ -0,0 +1,26 @@
+const {onlyOwner} = require('../../../../../../utils/access-control/onlyOwner');
+const lib = require('lib')({token: process.env.STDLIB_SECRET_TOKEN});
+
+await onlyOwner(context)
+
+await lib.discord.channels['@0.3.2'].messages.create({
+  "channel_id": `${context.params.event.channel_id}`,
+  "content": "",
+  "tts": false,
+  "embeds": [
+    {
+      "type": "rich",
+      "title": `Get Involved`,
+      "description": [
+        `Click on one of the emojis below to join a locality and help make the next Regens Unite gathering happen:`
+        `\n`,
+        `- 🇳🇱 Amsterdam, September 2022`,
+        `\n`,
+        `- 🇨🇴 Bogota, October 2022`,
+        `\n`,
+        `- 🇧🇪 Brussels, May 2023`,
+      ].join(''),
+      "color": 0x295846,
+    }
+  ]
+});
