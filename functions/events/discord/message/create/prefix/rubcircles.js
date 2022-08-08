882a3e22db5535b2045fe68e0ba8184a35a3759e
Bruno Roemers <bruno@roemers.io>
2022-08-08 21:50:32 +0200

    Create content for circles embed

--- functions/events/discord/message/create/prefix/rubcircles.js
+++ functions/events/discord/message/create/prefix/rubcircles.js
@@ -5,6 +5,15 @@ const lib = require('lib')({token: process.env.STDLIB_SECRET_TOKEN});
 
 await onlyOwner(context)
 
+const channels = await lib.discord.guilds['@0.2.4'].channels.list({
+  guild_id: `${context.params.event.guild_id}`
+})
+
+const convenantChannel = channels.find(channel => channel.name.includes('convenant'))
+if (!convenantChannel) {
+  throw new Error('could not find convenant channel')
+}
+
 console.log(context.params.event.content)
 
 const channelId = context.params.event.channel_id
@@ -16,8 +25,18 @@ await createOrUpdateMessage(lib, channelId, messageId, {
       "type": "rich",
       "title": `Join our DAO circles`,
       "description": [
-        `TODO`,
-      ].join('\n\n'),
+        `Click on one of the emojis below to join a DAO circle. The goal of each circle is to come up with proposals and make decisions in their topic area.`,
+        `\n`,
+        `\n`,
+        `- 📡 Communication, PR and Marketing`,
+        `\n`,
+        `- 🌱 Events`,
+        `\n`,
+        `- 🔅 DAO operations`,
+        `\n`,
+        `\n`,
+        `💡 Please sign the <#${convenantChannel.id}> first, if you haven't done so yet, otherwise the emojis will not work.`,
+      ].join(''),
       "color": 0x295846,
     }
   ]
